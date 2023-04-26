bc4ea79b80071c5514df4597cbb6921f90f106a6
LucheMP <luismonpla@outlook.com>
2023-04-26 10:40:47 -0500

    Agenda hecha con un recyclerview

---
+++ app/src/main/java/com/example/agenda09/Persona.kt
@@ -0,0 +1,6 @@
+package com.example.agenda09
+
+data class Persona(val nombre:String,
+                    val apellido:String,
+                    val edad:String,
+                    val id:String)
---
+++ app/src/main/java/com/example/agenda09/Provisional.kt
@@ -0,0 +1,7 @@
+package com.example.agenda09
+
+class Provisional {
+    companion object{
+        val listPersona = ArrayList<Persona>()
+    }
+}
